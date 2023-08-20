b28b43fca0eaf6fbd0b70a308d5648ec99ee95d8
hasan <hasan@pop-os.localdomain>
2023-08-21 02:40:14 +0300

    Commented out previous TDK API functionality due to API getting killed, wrote some basic scraping functionality for testing purposes.

--- sozluk.py
+++ sozluk.py
@@ -1,6 +1,33 @@
-import requests
+# import requests
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-URL = 'https://sozluk.gov.tr/gts_id?id='
+driver = webdriver.Firefox()
+driver.get("http://www.dildernegi.org.tr/TR,274/turkce-sozluk-ara-bul.html")
+
+elem = driver.find_element(By.ID, "txtSozlukText")
+elem.clear()
+elem.send_keys("a")
+elem.send_keys(Keys.RETURN)
+
+try:
+    res = WebDriverWait(driver, 5).until(
+        EC.presence_of_element_located((By.CLASS_NAME, "sozlukSonuc1"))
+    )
+    worde = driver.find_element(By.XPATH, '//*[@id="upSozluk"]/div/table/tbody/tr[1]/td[1]/a')
+    print(worde.text)
+    counter = 2
+    while worde is not None:
+        worde = driver.find_element(By.XPATH, '//*[@id="upSozluk"]/div/table/tbody/tr['+str(counter)+']/td[1]/a')
+        print(worde.text)
+        counter += 1
+finally:
+    driver.close()
+
+'''URL = 'https://sozluk.gov.tr/gts_id?id='
 headers = {
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36"
 }
@@ -32,4 +59,4 @@ def getAllWords (word_count, letter_limit):
     return wordList
 
 wordList = getAllWords(10, 3)
-print(wordList)
+print(wordList)'''
