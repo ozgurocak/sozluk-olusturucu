c2f59e2f20675c6a2a4f128813cf7ce23450850e
hasan <hasan@pop-os.localdomain>
2023-08-22 02:44:45 +0300

    Probably finalized scraping

--- sozluk.py
+++ sozluk.py
@@ -4,28 +4,51 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.firefox.options import Options
 
-driver = webdriver.Firefox()
+options = Options()
+options.headless = True
+driver = webdriver.Firefox(options=options)
 driver.get("http://www.dildernegi.org.tr/TR,274/turkce-sozluk-ara-bul.html")
 
-elem = driver.find_element(By.ID, "txtSozlukText")
-elem.clear()
-elem.send_keys("a")
-elem.send_keys(Keys.RETURN)
+def getWordsOfLetter(letter):
+    elem = driver.find_element(By.ID, "txtSozlukText")
+    elem.clear()
+    elem.send_keys(letter)
+    elem.send_keys(Keys.RETURN)
 
-try:
-    res = WebDriverWait(driver, 5).until(
-        EC.presence_of_element_located((By.CLASS_NAME, "sozlukSonuc1"))
-    )
+    words = []
+
+    WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, '//*[@id="divWait1"][contains(@style, "display: none")]')))
     worde = driver.find_element(By.XPATH, '//*[@id="upSozluk"]/div/table/tbody/tr[1]/td[1]/a')
     print(worde.text)
-    counter = 2
-    while worde is not None:
-        worde = driver.find_element(By.XPATH, '//*[@id="upSozluk"]/div/table/tbody/tr['+str(counter)+']/td[1]/a')
-        print(worde.text)
+    counter = 1
+    while True:
+        try:
+            for i in range(3):
+                worde = driver.find_element(By.XPATH, '//*[@id="upSozluk"]/div/table/tbody/tr['+str(counter)+']/td['+str(i+1)+']/a')
+                words.append(worde.text.split(",")[0].split(" ")[0])
+        except NoSuchElementException:
+            break
+
         counter += 1
-finally:
-    driver.close()
+    
+    return words
+
+def getAllWords():
+    words = []
+    letters = "abcçdefghıijklmnoöprsştuüvyz"
+    
+    for letter in letters:
+        wordslet = getWordsOfLetter(letter)
+        words += wordslet
+
+    driver.quit()
+
+    return words
+
+print(getAllWords())
 
 '''URL = 'https://sozluk.gov.tr/gts_id?id='
 headers = {
