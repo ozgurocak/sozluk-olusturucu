114be20b4c89c0876ed9f278793952bc1e69d8ab
hasan <hasan@pop-os.localdomain>
2023-08-22 20:25:42 +0300

    Now takes the size of the dictionary as a command line argument

--- sozluk.py
+++ sozluk.py
@@ -1,4 +1,3 @@
-# import requests
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
@@ -6,13 +5,14 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.firefox.options import Options
+import sys
 
 options = Options()
 options.headless = True
 driver = webdriver.Firefox(options=options)
 driver.get("http://www.dildernegi.org.tr/TR,274/turkce-sozluk-ara-bul.html")
 
-def getWordsOfLetter(letter):
+def getWordsOfLetter(letter, num):
     elem = driver.find_element(By.ID, "txtSozlukText")
     elem.clear()
     elem.send_keys(letter)
@@ -24,11 +24,16 @@ def getWordsOfLetter(letter):
     worde = driver.find_element(By.XPATH, '//*[@id="upSozluk"]/div/table/tbody/tr[1]/td[1]/a')
     print(worde.text)
     counter = 1
-    while True:
+    wordcount = 1
+    columns = 3
+    if num < columns:
+        columns = num
+    while wordcount <= num:
         try:
-            for i in range(3):
+            for i in range(columns):
                 worde = driver.find_element(By.XPATH, '//*[@id="upSozluk"]/div/table/tbody/tr['+str(counter)+']/td['+str(i+1)+']/a')
                 words.append(worde.text.split(",")[0].split(" ")[0])
+                wordcount += 1
         except NoSuchElementException:
             break
 
@@ -36,50 +41,27 @@ def getWordsOfLetter(letter):
     
     return words
 
-def getAllWords():
+def getAllWords(totalnum):
     words = []
     letters = "abcçdefghıijklmnoöprsştuüvyz"
     
-    for letter in letters:
-        wordslet = getWordsOfLetter(letter)
-        words += wordslet
+    if totalnum < len(letters):
+        for i in range(totalnum):
+            wordslet = getWordsOfLetter(letters[i], 1)
+            words += wordslet
+    else:
+        for letter in letters:
+            wordslet = getWordsOfLetter(letter, int(totalnum/len(letters)))
+            words += wordslet
+        if len(words) < totalnum:
+            for i in range(totalnum-(len(words)*len(letters))):
+                wordsfill = getWordsOfLetter(letters[i], 1)
+                words += wordsfill
 
     driver.quit()
 
-    return words
+    words = [word.lower() for word in words]
 
-print(getAllWords())
+    return sorted(words)
 
-'''URL = 'https://sozluk.gov.tr/gts_id?id='
-headers = {
-    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36"
-}
-entry_count = 99501
-
-def getWord (word_id):
-    res = requests.get(url = URL + str(word_id), headers = headers)
-    data = res.json()
-    return data[0]["madde"]
-
-def getAllWords (word_count, letter_limit):
-    wordList = []
-    
-    counter = 1
-    for c in range(entry_count+1):
-        if counter % int(entry_count/word_count) != 0:
-            counter = counter+1
-            c = c+1
-            continue
-        word = getWord(counter)
-        if len(word) >= letter_limit and ' ' not in word:
-            wordList.append(word)
-        else:
-            c = c-1
-        counter = counter+1
-    
-    wordList.sort()
-
-    return wordList
-
-wordList = getAllWords(10, 3)
-print(wordList)'''
+print(getAllWords(int(sys.argv[1])))
